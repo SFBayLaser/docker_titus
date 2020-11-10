e91594f2166b9eac3750aa282b29b18be8884195
Tracy Usher <usher@slac.stanford.edu>
2020-11-10 11:20:04 -0800

    Adding a setup file to get TITUS up and running

---
+++ setup_titus.sh
@@ -0,0 +1,13 @@
+# Basic setup for TITUS...
+# Note this is specific for the docker image and expects the folder structure it starts
+# in to contain a "larsoft" folder with the products and an "icarusalg" folder with the 
+# icarusalg package. In addition, it expects that the folder containing data will have 
+# been mounted as "data". 
+#
+# Basic setup of icarusalg
+source larsoft/products/setup
+setup mrb
+export MRB_PROJECT=icarusalg
+source icarusalg/local*/setup
+setup icarusalg v09_09_01 -q e19:prof
+source gallery-framework/config/setup_evd.sh
